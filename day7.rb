ad7b32df3e4ce8cd7de74f62b60487fc4fae3fc5
Sam ONeill <samuel.p.oneill@gmail.com>
2021-12-10 20:21:48 +1300

    adds comments

--- day7.rb
+++ day7.rb
@@ -1,18 +1,24 @@
-require 'pry'
+###
+# This is a bit shit, basically just brute force except for token attempt at 
+# being smart by exiting early if we can see that the fuel cost is already 
+# greater than the current minimum value. But it works.
+###
 
 class Day7
   def initialize(input = [16,1,2,0,4,2,7,1,2,14])
     @input    = input
     @min_fuel = nil
-    @shortest = nil
   end
   
   def solve
     positions_range.each do |converge_point|
-      fuel_usage = @input.each_with_object([]) do |position, arr|        
+      fuel_usage = @input.each_with_object([]) do |position, arr|
         arr << trip_fuel(converge_point, position)
-      end.sum
-      add_min_fuel! fuel_usage
+        if @min_fuel
+          break if arr.sum > @min_fuel
+        end
+      end
+      add_min_fuel!(fuel_usage.sum) if fuel_usage
     end
     @min_fuel
   end
@@ -37,7 +43,3 @@ class Day7
 end
 
 puts Day7.new(File.read('./inputs/day7.txt').split(',').map(&:to_i)).solve
-# d = Day7.new
-# d.solve
-# binding.pry
-
